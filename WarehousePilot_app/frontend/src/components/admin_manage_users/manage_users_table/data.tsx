c17701ed72a00ad49920301efb705c1680207739
yousefenein <yousefenein@outlook.com>
2025-01-04 12:04:51 -0500

    Implemented updated UI to integrrate with edit/adding Users

---
+++ WarehousePilot_app/frontend/src/components/admin_manage_users/manage_users_table/data.tsx
@@ -0,0 +1,100 @@
+import {DangerCircleSvg} from "./danger-circle";
+import {DefaultCircleSvg} from "./default-circle";
+import {SuccessCircleSvg} from "./success-circle";
+import {WarningCircleSvg} from "./warning-circle";
+import axios from "axios";
+
+export const statusOptions = [
+  {name: "Active", uid: "active"},
+  {name: "Inactive", uid: "inactive"},
+  {name: "Paused", uid: "paused"},
+  {name: "Vacation", uid: "vacation"},
+] as const;
+
+export type StatusOptions = (typeof statusOptions)[number]["name"];
+
+export const statusColorMap: Record<StatusOptions, JSX.Element> = {
+  Active: SuccessCircleSvg,
+  Inactive: DefaultCircleSvg,
+  Paused: DangerCircleSvg,
+  Vacation: WarningCircleSvg,
+};
+
+export const roleOptions = [
+  {name: "All", uid: "all"},
+  {name: "Manager", uid: "manager"},
+  {name: "Admin", uid: "admin"},
+] as const;
+
+export const departmentOptions = [
+  {name: "All", uid: "all"},
+  {name: "IT", uid: "it"},  
+] as const;
+
+export type UserInfo = {
+  avatar: string;
+  email: string;
+  name: string;
+};
+
+export type Users = {
+  user_id: number;
+  first_name: string;
+  last_name: string;
+  staffID: string;
+  email: string;
+  role: string;
+  department: string;
+};
+
+export type ColumnsKey =
+  | "first_name"
+  | "last_name"
+  | "staffID"
+  | "email"
+  | "role"
+  | "department"
+  | "actions";
+
+export const INITIAL_VISIBLE_COLUMNS: ColumnsKey[] = [
+  "first_name",
+  "last_name",
+  "staffID",
+  "email",
+  "role",
+  "department",
+  "actions",
+];
+
+export const columns = [
+  {name: "First Name", uid: "first_name"},
+  {name: "Last Name", uid: "last_name"},
+  {name: "Staff ID", uid: "staffID"},
+  {name: "Email", uid: "email"},
+  {name: "Role", uid: "role"},
+  {name: "Department", uid: "department"},
+  {name: "Actions", uid: "actions"},
+];
+
+export const fetchUserInfo = async (): Promise<Users[]> => {
+  try {
+    const response = await axios.get('http://127.0.0.1:8000/admin_dashboard/manage_users', {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    if (response.status !== 200) {
+      throw new Error("Failed to fetch user info");
+    }
+    const data = response.data;
+    console.log("Fetched data:", data); // Log the fetched data
+    if (!Array.isArray(data)) {
+      throw new Error("Response data is not an array");
+    }
+    return data.map((item: any) => ({
+      ...item,
+      staffID: item.user_id,
+    }));
+  } catch (error) {
+    console.error('Fetching user info failed:', error);
+    throw error;
+  }
+};
