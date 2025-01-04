0bbf2c9894bdeb995e2a2838b2cde05171d0fd4f
iluvpesto <102493688+NourHadieHassoun@users.noreply.github.com>
2025-01-04 18:27:46 -0500

    Fixing checks failed

--- WarehousePilot_app/frontend/jest.config.cjs
+++ WarehousePilot_app/frontend/jest.config.cjs
@@ -2,7 +2,8 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   moduleNameMapper: {
-    '\\.(css|less)$': 'identity-obj-proxy'
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(svg)$': '<rootDir>/jest-svg-mock.js '
   },
   transform: {
     '^.+\\.(js|jsx)$': 'babel-jest'
