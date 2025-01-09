145364bf5814e78eed11111931a3cd0825eae3d1
James Bitharas <jamesbitharas@gmail.com>
2025-01-09 10:39:21 -0500

    Update dashboard_routes.jsx
    #107 Updated the dashboard_routes to have admin only elements be children of the Admin Dashboard frontend. This way, role verification need only be done once in Admin_dashboard.jsx.
    
    To Do:
    - Add tests to ensure functionality
    - Confirm if there are any missing paths that need to be directed to the admin dashboard

--- WarehousePilot_app/frontend/src/components/routes/dashboard_routes.jsx
+++ WarehousePilot_app/frontend/src/components/routes/dashboard_routes.jsx
@@ -9,22 +9,28 @@ import OrderListView from '../orders/OrderListView';
 import ManageUsersTable from '../admin_manage_users/manage_users_table/App';
 import InventoyPickList from '../orders/InventoyPickList';
 import InventoryPickListItems from '../orders/InventoryPickListItems';
+import { chip } from '@nextui-org/react';
 
 
 export const dashboard_routes = [
   { path: '/dashboard', element: <Dashboard /> },
   { path: '/manager_dashboard', element: <ManagerDashboard /> },
-  { path: '/admin_dashboard', element: <AdminDashboard /> },
-  { path: '/admin_dashboard/manage_users', element: <ManageUsersTable /> },
+
+  { path: '/admin_dashboard', element: <AdminDashboard />, 
+    children: [ 
+      { path: 'manage_users', element: <ManageUsersTable /> },
+      { path: 'add_users', element: <AddUsersDashboard />},
+      { path: 'edit_user/:user_id', element: <AddUsersDashboard />}
+    ]
+   },
+  
   { path: '/account_management', element: <AccountManagement /> },
   { path: '/profile', element: <Profile /> },
   { path: '/kpi', element: <KpiDashboard /> },
   { path: '/change_password', element: <ChangePassword /> },
   { path: '/inventory-stock', element: <InventoryTable /> },
-  {path: '/admin_dashboard/add_users', element: <AddUsersDashboard />},
+  // {path: '/admin_dashboard/add_users', element: <AddUsersDashboard />}, duplicate to be removed
   { path: '/navbar', element: <NavBar /> },
-  {path: '/admin_dashboard/edit_user/:user_id', element: <AddUsersDashboard />},
-  { path: '/admin_dashboard/add_users', element: <AddUsersDashboard />},
   { path: '/orders', element: <OrderListView /> },
   { path: '/inventory_pick_list', element: <InventoyPickList /> },
   { path: '/inventory_picklist_items/:order_id', element: <InventoryPickListItems /> },
