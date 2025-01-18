8707b2e665da63c6a3a66ee4891eed923fa14aae
yousefenein <yousefenein@outlook.com>
2025-01-18 02:19:47 -0500

    #168 - Updated API calls for staff dashboard

--- WarehousePilot_app/frontend/src/components/dashboard/StaffDashboard.jsx
+++ WarehousePilot_app/frontend/src/components/dashboard/StaffDashboard.jsx
@@ -6,6 +6,8 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Dashboard from './Dashboard';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 function StaffDashboard() {
   const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
@@ -15,7 +17,7 @@ function StaffDashboard() {
       const token = localStorage.getItem('token');
       if (token) {
         try {
-          const response = await axios.get('http://127.0.0.1:8000/auth/profile/', {
+          const response = await axios.get(`${API_BASE_URL}/auth/profile/`, {
             headers: { Authorization: `Bearer ${token}` },
           });
           setUserData(response.data);
