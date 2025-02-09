23e79f2dfb0655447c81e5269dabf86dc4f60ac7
suha-ab <88583460+suha-ab@users.noreply.github.com>
2025-02-08 23:21:45 -0500

    working on #248 #249

---
+++ WarehousePilot_app/frontend/src/components/kpis/CTPO/CTPO_main.jsx
@@ -0,0 +1,28 @@
+// CTPO: Cycle Time Per Order
+// This page displays more in-depth information about cycle time per order
+
+
+import React, { useState } from "react";
+import SideBar from "../../dashboard_sidebar1/App";
+
+const CTPO = () => {
+
+  return (
+    <div className="h-full">
+        <div className="flex-1">
+            <SideBar />
+            
+            <main className="flex p-6 h-screen justify-center">
+                Hi
+            </main>
+
+
+        </div>
+
+
+    </div>
+  );
+};
+
+export default CTPO;
+
