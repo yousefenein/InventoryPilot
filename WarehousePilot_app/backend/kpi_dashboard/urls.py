2b87b768bdd0a30ea4d69da8979258904e5379a2
flo <79473110+flo351@users.noreply.github.com>
2025-02-25 09:52:27 -0500

    register daily picks stats and details endpoints 3.2 Order Picking Accuracy  #183.2.2 Add an interface to view accuracy reports. #257

--- WarehousePilot_app/backend/kpi_dashboard/urls.py
+++ WarehousePilot_app/backend/kpi_dashboard/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import daily_picks_data, daily_picks_details
 
 urlpatterns = [
     path('order-picking-accuracy/', views.order_picking_accuracy, name='order_picking_accuracy'),
+    path('order-picking-daily-stats/', daily_picks_data, name='daily_picks_data'),
+    path('order-picking-daily-details/', daily_picks_details, name='daily_picks_details'),
 ]
