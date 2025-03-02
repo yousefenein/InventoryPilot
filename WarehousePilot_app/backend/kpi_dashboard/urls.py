24b92d3226cb5214c6d9ce84a5ff372efd8ac9ac
Fatoumatabintabarry <97992954+Fatoumatabintabarry@users.noreply.github.com>
2025-03-01 20:04:53 -0500

    Backend API endpoint for order fullfilement rate graph
    d

--- WarehousePilot_app/backend/kpi_dashboard/urls.py
+++ WarehousePilot_app/backend/kpi_dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import daily_picks_data, daily_picks_details
+from .views import daily_picks_data, daily_picks_details, order_fulfillment_rate
 
 urlpatterns = [
     path('order-picking-accuracy/', views.order_picking_accuracy, name='order_picking_accuracy'),
     path('order-picking-daily-stats/', daily_picks_data, name='daily_picks_data'),
     path('order-picking-daily-details/', daily_picks_details, name='daily_picks_details'),
+    path('order-fulfillment-rate/', order_fulfillment_rate, name='order_fulfillment_rate'),  # New endpoint
 ]
