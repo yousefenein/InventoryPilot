11dbb5a91b449ef155d82dca5b9e4bf68ccc6f53
m-coscia <m_coscia@live.concordia.ca>
2025-03-27 15:52:17 -0400

    #333 Finished implementing backend API for Inventory Picking Logs

--- WarehousePilot_app/backend/log_actions/views.py
+++ WarehousePilot_app/backend/log_actions/views.py
@@ -1,18 +1,21 @@
+# This file defines views for employee activity logging 
+# InventoryPickingLogging(): Track the picking activity of employees in the warehouse
+
 import logging
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from inventory.models import InventoryPicklist, InventoryPicklistItem
+from inventory.models import InventoryPicklist, InventoryPicklistItem, Inventory
+from rest_framework import status
 
 # Django logger for backend
 logger = logging.getLogger('WarehousePilot_app')
 
-# Create your views here.
 class InventoryPickingLogging(APIView):
-    #authentication_classes = [JWTAuthentication]
-    #permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         # Initialize the list of picklist items
@@ -20,51 +23,58 @@ class InventoryPickingLogging(APIView):
 
         try:
             # Fetch all the picklists
-            picklists = InventoryPicklist.objects.all().values('picklist_id', 'warehouse_nb', 'assigned_employee_id')
+            picklists = InventoryPicklist.objects.all().values('picklist_id', 'warehouse_nb', 'assigned_employee_id').filter(assigned_employee_id__isnull=False).order_by('-picklist_id')
 
             try:
                 for picklist in picklists:
-                    logger.info(f"Picklist: {picklist}")
                     # Set the values for the following variables
                     id = picklist['picklist_id']
-                    warehouse = picklist['warehouse_nb']
+                    warehouse = '499' if picklist['warehouse_nb'] is None else picklist['warehouse_nb']
                     employee_id = picklist['assigned_employee_id']
-                    logger.info(f"Picklist ID: {id}, Warehouse: {warehouse}, Employee ID: {employee_id}")
 
                     # Fetch all the picklist items for each picklist
-                    picklist_items = InventoryPicklistItem.objects.filter(picklist_id=id).values('item_picked_timestamp', 'location', 'sku_color', 'amount')
+                    picklist_items = InventoryPicklistItem.objects.all().filter(picklist_id=id, status=True).values('picked_at', 'location', 'sku_color', 'amount')
 
-                    # Loop through each picklist item
+                    # Formatting picklist item information to be returned in picked_items
                     for item in picklist_items:
-                        picked_items.append({
-                            'warehouse': warehouse,
-                            'date': item['item_picked_timestamp'].date(),
-                            'time': item['item_picked_timestamp'],
-                            'employee_id': employee_id,
-                            'transaction_type': 'Picking',
-                            'sku_color': item['sku_color'],
-                            'location': item['location'],
-                            'qty_out': item['amount']
-                        })
+                        try:
+                            # Fetch the location for each item
+                            location = Inventory.objects.all().filter(sku_color_id=item['sku_color'], location__isnull=False).values('location').first()
+                           
+                            # Add the item to the list of picked items
+                            picked_items.append({
+                                'warehouse': warehouse,
+                                'date': None if item['picked_at'] is None else item['picked_at'].date(),
+                                'time': None if item['picked_at'] is None else item['picked_at'],
+                                'employee_id': employee_id,
+                                'transaction_type': 'Picking',
+                                'sku_color': item['sku_color'],
+                                'location': location['location'] if item['location'] is None else item['location'],
+                                'qty_out': item['amount']
+                            })
+
+                        except Exception as e:
+                            logger.error(f"Failed to fetch information for picklist item with sku_color of {item['sku_color']} (InventoryPickingLogging)")
                 
                 # Sort the list of picklist items by date annd time in descending order
-                picked_items.sort(key=lambda x: x['time'], reverse=True)
+                picked_items = sorted(picked_items, key=lambda x: (x['time'] is not None, x['time']), reverse=True)
 
-                logger.info(f"Inventory Picking Logging: {picked_items}")
-                return Response(picked_items)
+                # Return the list of picked items
+                logger.info(f"Successfully fetch data for tracking inventory picking activity (InventoryPickingLogging)")
+                return Response(picked_items, status=status.HTTP_200_OK)
+            
+            except InventoryPicklist.DoesNotExist:
+                logger.error("Inventory Picklist does not exist")
+                return Response(status=status.HTTP_404_NOT_FOUND)
+            
+            except InventoryPicklistItem.DoesNotExist:
+                logger.error("Inventory Picklist Item does not exist")
+                return Response(status=status.HTTP_404_NOT_FOUND)
             
             except Exception as e:
-                logger.error("Failed to get info on the individual picklist (InventoryPickingLogging)")
-                return Response({"error": str(e)}, status=500)
-            
-        except InventoryPicklist.DoesNotExist:
-            logger.error("Inventory Picklist does not exist")
-            return Response(status=404)
-        
-        except InventoryPicklistItem.DoesNotExist:
-            logger.error("Inventory Picklist Item does not exist")
-            return Response(status=404)
-        
+                logger.error("Failed to fetch data for the picklist %s (InventoryPickingLogging)", id)
+                return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
+
         except Exception as e:
-            logger.error("Failed to fetch data tracking inventory picking (InventoryPickingLogging)")
-            return Response({"error": str(e)}, status=500)
+            logger.error("Failed to fetch the data for tracking inventory picking activity (InventoryPickingLogging)")
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
